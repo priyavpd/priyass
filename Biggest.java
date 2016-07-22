2dde8dcb84edb6e42ffa3cafa88ab0117b3b3871
priyavpd <vpriyacse2@gmail.com>
2016-07-22 14:58:59 +0530

    Create Biggest.java

---
+++ Biggest.java
@@ -0,0 +1,25 @@
+# priyass
+public class Biggest
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter the n1");
+int a=s.nextInt();
+System.out.println("enter the n2");
+int b=s.nextInt();
+System.out.println("enter the n3");
+int c=s.nextInt();
+if((a>b) && (a>c))
+{
+System.out.println("a is big");
+}
+else if(b>c)
+{
+System.out.println("b is big");
+}
+else
+{
+System.out.println("c is big");
+}}
+}
